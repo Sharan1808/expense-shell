0dd8cb1841a87479e821ee8bb0355fe490c2240b
Sharan <sharanpyata1808@gmail.com>
2023-11-07 01:38:18 -0600

    Adding shell topics

--- mysql.sh
+++ mysql.sh
@@ -1,23 +1,29 @@
-log_file="/tmp/expense.log"
-color="\e[33m"
+source common.sh
+
+if [ -z "$1" ]; then
+  echo Password is Missing
+  exit
+fi
+
+MYSQL_ROOT_PASSWORD=$1
 
 echo -e "${color} Disable MySQL default version \e[0m"
 dnf module disable mysql -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Copy MySQL Repo File \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Install MySQL Server \e[0m"
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Start MySQL Server \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Set MySQL Password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo $?
+mysql_secure_installation --set-root-pass ${MYSQL_ROOT_PASSWORD} &>>$log_file
+status_check
